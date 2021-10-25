409abe1af61499dd9f6214500a49ea066c3e85ce
langehk <32093132+langehk@users.noreply.github.com>
2021-10-25 22:40:43 +0200

    Cleanup in Identity, and created initial migration for database

--- Ferieboliger.DAL/Context/FerieboligDbContext.cs
+++ Ferieboliger.DAL/Context/FerieboligDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,17 +10,27 @@ using System.Threading.Tasks;
 
 namespace Ferieboliger.DAL.Context
 {
-    public class FerieboligDbContext : IdentityDbContext
+    public class FerieboligDbContext : DbContext
     {
-        public FerieboligDbContext(DbContextOptions<FerieboligDbContext> options)
-            : base(options)
+        public FerieboligDbContext(DbContextOptions<FerieboligDbContext> options) : base(options)
         {
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer("Server=tcp:blazorgolf.database.windows.net,1433;Initial Catalog=WebApp_db;Persist Security Info=False;User ID=langehk;Password=Tigerwoods6!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=Ferieboliger;Trusted_Connection=True;");
+            }
+        }
         public virtual DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            base.OnModelCreating(modelBuilder);
+
             // User
             modelBuilder.Entity<User>(x =>
             {
