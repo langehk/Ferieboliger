8babc669edc13fb2a8bf3573632c58c90d7bff26
langehk <32093132+langehk@users.noreply.github.com>
2021-11-09 11:47:51 +0100

    [Ferieboliger] Created PdfDocument class - display file.Name and created link for file.id (apiController.)

---
+++ Ferieboliger.DAL/Models/PdfDocument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferieboliger.DAL.Models
+{
+    public class PdfDocument
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
