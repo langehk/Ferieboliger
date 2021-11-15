de1bb4762eeb4cc63d5df59cc3f26fb946c0ec2b
langehk <32093132+langehk@users.noreply.github.com>
2021-11-15 09:36:40 +0100

    [Ferieboliger] Filtrering.

---
+++ Ferieboliger/ViewModel/FiltreretFeriebolig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ferieboliger.ViewModel
+{
+    public class FiltreretFeriebolig
+    {
+        public HashSet<string> Byer { get; set; }
+
+        public List<string> BoligType { get; set; }
+
+        public DateTime StartDato { get; set; }
+        public DateTime SlutDato { get; set; }
+    }
+}
