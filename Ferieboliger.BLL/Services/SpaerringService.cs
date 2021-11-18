d69be2a27303035db4fe4e5c973e3644e4f4a8e9
nadiaskau <nadiaskau@hotmail.com>
2021-11-18 14:27:38 +0100

    sletning af spærring

--- Ferieboliger.BLL/Services/SpaerringService.cs
+++ Ferieboliger.BLL/Services/SpaerringService.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Ferieboliger.DAL.Models;
 using Ferieboliger.DAL.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ferieboliger.BLL.Services
 {
     public interface ISpaerringService
     {
         Task<Spaerring> CreateSpaerringAsync(Spaerring spaerring);
+
+        Task<Spaerring> DeleteSpaerringByIdAsync(int id);
     }
     public class SpaerringService : ISpaerringService
     {
@@ -34,6 +37,29 @@ namespace Ferieboliger.BLL.Services
                 throw new Exception(ex.Message);
             }
             
+        }
+
+        public async Task<Spaerring> DeleteSpaerringByIdAsync(int id)
+        {
+            try
+            {
+                var spaerring = await dbContext.Spaerringer.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+                if (spaerring == null)
+                {
+                    // TODO - Better errorhandling, shouldn't be displayed in the console.
+                    Console.WriteLine($"Booking with id {id} not found");
+                }
+
+                dbContext.Spaerringer.Remove(spaerring);
+                await dbContext.SaveChangesAsync();
+                return spaerring;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
