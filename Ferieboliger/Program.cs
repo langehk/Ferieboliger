4a6c584dee488688098ba1309e1a8dbe391a91b0
langehk <32093132+langehk@users.noreply.github.com>
2022-01-04 09:11:10 +0100

    Added Serilog logging. - logging to file.

--- Ferieboliger/Program.cs
+++ Ferieboliger/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,11 +18,14 @@ namespace Ferieboliger
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration().Enrich.FromLogContext().WriteTo.File(@"C:\logs\log.txt", 
+                restrictedToMinimumLevel : Serilog.Events.LogEventLevel.Error, 
+                outputTemplate: "{Timestamp:yyyy-mm-dd HH:mm:ss zzz}[{Level:u3}]{Message:lj}{NewLine}{Exception}").CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args).UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
